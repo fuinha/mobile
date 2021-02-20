85139530ca96d068304462f6735af44838271cbf
lucaszcai <lucas.z.cai@gmail.com>
2021-02-20 09:26:22 -0600

    set up basic tab view for dashboard

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:petcode_app/utils/style_constants.dart';
 
 class DashboardScreen extends StatefulWidget {
   @override
@@ -8,9 +10,60 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Dashboard Screen'),
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: height * 0.15,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Container(
+            height: 1000.0,
+            color: Colors.blue,
+
+          ),
+          bottom: TabBar(
+            unselectedLabelColor: StyleConstants.grey,
+            labelColor: StyleConstants.pcBlue,
+            indicatorColor: StyleConstants.pcBlue,
+            indicatorPadding: EdgeInsets.symmetric(horizontal: width * 0.1),
+            tabs: [
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Events', style: StyleConstants.regTextLarge),
+                    SizedBox(width: width * 0.03,),
+                    FaIcon(FontAwesomeIcons.paw,),
+                  ],
+                ),
+              ),
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Upcoming', style: StyleConstants.regTextLarge),
+                    SizedBox(width: width * 0.03,),
+                    FaIcon(FontAwesomeIcons.calendar,),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: Text('Events'),
+            ),
+            Center(
+              child: Text('Upcoming'),
+            )
+          ],
+        )
       ),
     );
   }
--- lib/screens/root_screen.dart
+++ lib/screens/root_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:petcode_app/screens/discovery_screen.dart';
 import 'package:petcode_app/screens/health_screen.dart';
 import 'package:petcode_app/screens/safety_screen.dart';
@@ -20,14 +21,59 @@ class _RootScreenState extends State<RootScreen> {
     DiscoveryScreen()
   ];
 
-  double height = StyleConstants.height;
+  //double height = StyleConstants.height;
+
+  int currentIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
 
-      body: Center(
-        child: Text('Root Screen',),
+
+
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: _pageOptions,
+      ),
+      bottomNavigationBar: SizedBox(
+        //height: height * 0.12,
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: currentIndex,
+          onTap: (index) => _onItemTapped(index),
+          selectedItemColor: StyleConstants.pcBlue,
+          backgroundColor: Colors.white,
+          unselectedItemColor: Colors.black.withOpacity(0.2),
+          unselectedIconTheme: IconThemeData(size: height * 0.028),
+          selectedIconTheme: IconThemeData(size: height * 0.028),
+          showUnselectedLabels: true,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.shieldAlt),
+              label: 'Safety',
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.heartbeat),
+              label: 'Health',
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.search),
+              label: 'Discovery',
+            ),
+          ],
+        ),
       ),
     );
   }
